5d91a380d45a9ef5831b59f80e62667a46bdc1c0
abtcodermilli <thapaankur2000@gmail.com>
2026-02-23 11:04:26 +0530

    UC5: Refactored banner using inline array initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,47 +1,52 @@
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
         // Letter O (7x9)
-        String O1 = "  *****  ";
-        String O2 = " *     * ";
-        String O3 = " *     * ";
-        String O4 = " *     * ";
-        String O5 = " *     * ";
-        String O6 = " *     * ";
-        String O7 = "  *****  ";
+        String[] O = {
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
 
         // Letter P (7x9)
-        String P1 = " ******* ";
-        String P2 = " *     * ";
-        String P3 = " *     * ";
-        String P4 = " ******* ";
-        String P5 = " *       ";
-        String P6 = " *       ";
-        String P7 = " *       ";
+        String[] P = {
+                " ******* ",
+                " *     * ",
+                " *     * ",
+                " ******* ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
 
         // Letter S (7x9)
-        String S1 = "  ****** ";
-        String S2 = " *       ";
-        String S3 = " *       ";
-        String S4 = "  *****  ";
-        String S5 = "       * ";
-        String S6 = "       * ";
-        String S7 = " ******  ";
+        String[] S = {
+                "  ****** ",
+                " *       ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                "       * ",
+                " ******  "
+        };
 
-        // Create String Array to store all banner lines
-        String[] banner = new String[7];
+        // Inline array initialization with String.join()
+        String[] banner = {
+                String.join(" ", O[0], O[0], P[0], S[0]),
+                String.join(" ", O[1], O[1], P[1], S[1]),
+                String.join(" ", O[2], O[2], P[2], S[2]),
+                String.join(" ", O[3], O[3], P[3], S[3]),
+                String.join(" ", O[4], O[4], P[4], S[4]),
+                String.join(" ", O[5], O[5], P[5], S[5]),
+                String.join(" ", O[6], O[6], P[6], S[6])
+        };
 
-        // Populate array using String.join()
-        banner[0] = String.join(" ", O1, O1, P1, S1);
-        banner[1] = String.join(" ", O2, O2, P2, S2);
-        banner[2] = String.join(" ", O3, O3, P3, S3);
-        banner[3] = String.join(" ", O4, O4, P4, S4);
-        banner[4] = String.join(" ", O5, O5, P5, S5);
-        banner[5] = String.join(" ", O6, O6, P6, S6);
-        banner[6] = String.join(" ", O7, O7, P7, S7);
-
-        // Print banner using enhanced for-loop
+        // Print using enhanced for-loop
         for (String line : banner) {
             System.out.println(line);
         }
