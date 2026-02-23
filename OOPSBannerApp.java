e402500e5fe7313c01b8bcd23adec6b0bbd6ac57
abtcodermilli <thapaankur2000@gmail.com>
2026-02-23 10:34:42 +0530

    UC4: Refactored banner using String array and enhanced for-loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -29,13 +29,21 @@ public class OOPSBannerApp {
         String S6 = "       * ";
         String S7 = " ******  ";
 
-        // Print O O P S using String.join()
-        System.out.println(String.join(" ", O1, O1, P1, S1));
-        System.out.println(String.join(" ", O2, O2, P2, S2));
-        System.out.println(String.join(" ", O3, O3, P3, S3));
-        System.out.println(String.join(" ", O4, O4, P4, S4));
-        System.out.println(String.join(" ", O5, O5, P5, S5));
-        System.out.println(String.join(" ", O6, O6, P6, S6));
-        System.out.println(String.join(" ", O7, O7, P7, S7));
+        // Create String Array to store all banner lines
+        String[] banner = new String[7];
+
+        // Populate array using String.join()
+        banner[0] = String.join(" ", O1, O1, P1, S1);
+        banner[1] = String.join(" ", O2, O2, P2, S2);
+        banner[2] = String.join(" ", O3, O3, P3, S3);
+        banner[3] = String.join(" ", O4, O4, P4, S4);
+        banner[4] = String.join(" ", O5, O5, P5, S5);
+        banner[5] = String.join(" ", O6, O6, P6, S6);
+        banner[6] = String.join(" ", O7, O7, P7, S7);
+
+        // Print banner using enhanced for-loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
